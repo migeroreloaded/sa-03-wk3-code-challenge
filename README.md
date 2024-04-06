d688c51d6dc79f5752560b552f54390e7be5ce64
Migero <mikeroche138@gmail.com>
2024-04-06 14:17:30 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Flatdango
+
+This is a movie ticketing app by *Michael George* first created on 05/04/2024
+
+## Description
+
+This is an application, Flatdango, that allows a user to purchase movie tickets from the
+theater.
+
+## Demo
+
+![Flatdango App Demo](./assets/flatdangoDemo.gif)
+
+## How to Run
+
+### Requirements
+
+* Browser
+* Computer with a connection to the internet
+
+### Installation Process
+
+1. Clone this repository using
+
+    ```bash
+      git clone # code link
+    ```
+
+    or by downloading a ZIP file of the code.
+  
+2. The repository, if downloaded as a .zip file will need to be extracted to your preferred location.
+
+3. Navigate to the project folder on your bash terminal.
+
+4. Install dependancies using
+
+    ```bash
+      npm install
+    ```
+
+5. Run this command to get the backend/terminal started:
+
+    ```console
+      json-server --watch db.json
+    ```
+
+6. Then, open the `index.html` file on your browser to run the application.
+
+## Technolgies used
+
+*
+*
+
+## Contact
+
+For any questions or concerns please reach out to me at:  
+[Email](mailto:mikeroche138@gmail.com)
+
+## License
+
+The content of this site is licensed under the MIT license
+
+Copyright &copy; 2024 Migero
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/index.js
@@ -0,0 +1,136 @@
+// Your code here
+document.addEventListener('DOMContentLoaded', () => {
+    // Assigning DOM elements to variables for easy access
+    const movieTitle = document.getElementById('films');
+    const poster = document.getElementById('poster');
+    const title = document.getElementById('title');
+    const runtime = document.getElementById('runtime');
+    const description = document.getElementById('film-info');
+    const showtime = document.getElementById('showtime');
+    const ticketNum = document.getElementById('ticket-num');
+    const buyButton = document.getElementById('buy-ticket');
+
+    // Fetch film data from the server
+    fetch('http://localhost:3000/films')
+    .then(response => response.json())
+    .then(films => {
+        // Clearing the existing movie title list
+        movieTitle.innerHTML = '';
+        // Looping through each film fetched from the server
+        films.forEach((film) => {
+            // Creating a list item for each film
+            const li = document.createElement('li');
+            // Adding CSS classes to the list item
+            li.classList.add('film', 'item');
+            // Setting the text content of the list item to the film title
+            li.textContent = film.title;
+            // Setting a unique ID for each film item
+            li.id = `film-${film.id}`;
+            // Appending the list item to the movie title list
+            movieTitle.appendChild(li);
+            // Add "sold-out" class if tickets are sold out
+            if (film.capacity - film.tickets_sold === 0) {
+                li.classList.add('sold-out');
+            }
+
+            // Adding an event listener to each list item to display film details when clicked
+            li.addEventListener('click', () => displayFilmDetails(film));
+
+            // Add delete button next to each film
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.classList.add('delete-button');
+            deleteButton.addEventListener('click', (event) => {
+                event.stopPropagation(); // Prevent li click event from firing
+                deleteFilm(film.id);
+            });
+            li.appendChild(deleteButton);
+        });
+        // Displaying details of the first film in the list by default
+        if (films.length > 0) {
+            displayFilmDetails(films[0]);
+        }
+    })
+    .catch(error => console.error('Error fetching films:', error));
+
+    // Function to display film details
+    function displayFilmDetails(film) {
+        // Setting the poster source, title, runtime, description, showtime, and ticket number
+        poster.src = film.poster;
+        title.textContent = film.title;
+        runtime.textContent = `${film.runtime} minutes`;
+        description.textContent = film.description;
+        showtime.textContent = film.showtime;
+        ticketNum.textContent = film.capacity - film.tickets_sold;
+
+        // Update buy button based on availability
+        if (film.capacity - film.tickets_sold > 0) {
+            buyButton.textContent = 'Buy Ticket';
+            buyButton.disabled = false;
+        } else {
+            buyButton.textContent = 'Sold Out';
+            buyButton.disabled = true;
+        }
+
+        // Adding an event listener to the buy button to execute buyTicket function with the current film
+        buyButton.removeEventListener('click', buyTicket); // Remove previous event listener
+        buyButton.addEventListener('click', () => buyTicket(film));
+    };
+
+    // Function to buy a ticket
+    function buyTicket(film) {
+        // Parsing the available ticket number from the ticketNum element
+        let availableTickets = parseInt(ticketNum.textContent);
+        // Checking if there are available tickets
+        if (availableTickets > 0) {
+            // Decreasing the available ticket count by one
+            availableTickets--;
+            // Updating the displayed ticket number
+            ticketNum.textContent = availableTickets;
+            // Sending a PATCH request to update tickets_sold for the current film
+            fetch(`http://localhost:3000/films/${film.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    tickets_sold: film.tickets_sold + 1
+                })
+            })
+            .then(response => response.json())
+            .then(updatedFilm => {
+                // Updating the film object with the updated tickets_sold value
+                film.tickets_sold = updatedFilm.tickets_sold;
+                console.log('Ticket purchased successfully!');
+
+                // Update buy button based on availability
+                if (availableTickets === 0) {
+                    buyButton.textContent = 'Sold Out';
+                    buyButton.disabled = true;
+                }
+
+            })
+            .catch(error => console.error('Error purchasing ticket:', error));
+        } else {
+            // Alerting the user if there are no available tickets
+            alert('There are no available tickets for this film. Please try another film.');
+        }
+    }
+
+    // Function to delete a film
+    function deleteFilm(filmId) {
+        fetch(`http://localhost:3000/films/${filmId}`, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(() => {
+            // Remove the film from the UI
+            const filmItem = document.getElementById(`film-${filmId}`);
+            if (filmItem) {
+                filmItem.remove();
+            }
+            console.log('Film deleted successfully!');
+        })
+        .catch(error => console.error('Error deleting film:', error));
+    }
+});
